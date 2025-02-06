eafa5ee55154f21e78d9ed28fc5af5470e1c5502
Dilan Swami <daswami100@gmail.com>
2025-02-06 10:27:22 -0500

    Tweaked start program

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start Gunicorn with optimizations
-gunicorn myproject.wsgi:application --workers=3 --worker-class=gevent --bind 0.0.0.0:10000
+gunicorn myproject.wsgi:application --workers=3 --bind 0.0.0.0:10000
